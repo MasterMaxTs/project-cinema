4146159882e48f5ad75bd6f9a25087758c4ef3a8
Maxim Tsurkanov <max86ts@gmail.com>
2022-07-18 23:10:36 +0300

    2. Сервис - инотеатр [#504869 #322328].Refactoring

--- src/main/java/ru/job4j/cinema/services/PlaceService.java
+++ src/main/java/ru/job4j/cinema/services/PlaceService.java
@@ -1,5 +1,6 @@
 package ru.job4j.cinema.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -11,8 +12,18 @@ import java.util.stream.IntStream;
 @Service
 public class PlaceService {
 
-    private static final int ROWS_COUNT = 3;
-    private static final int CELLS_COUNT = 3;
+    private static int rowsCount;
+    private static int cellsCount;
+
+    @Value("${rows.count}")
+    public void setRowsCount(String rows) {
+        PlaceService.rowsCount = Integer.parseInt(rows);
+    }
+
+    @Value("${cells.count}")
+    public void setCellsCount(String cells) {
+        PlaceService.cellsCount = Integer.parseInt(cells);
+    }
 
     public static Map<Integer, List<Integer>> initMapFreeSeats() {
         Map<Integer, List<Integer>> rsl = new HashMap<>();
@@ -24,13 +35,13 @@ public class PlaceService {
     }
 
     public static List<Integer> rows() {
-        return IntStream.range(1, ROWS_COUNT + 1)
+        return IntStream.range(1, rowsCount + 1)
                 .boxed()
                 .collect(Collectors.toList());
     }
 
     public static List<Integer> cells() {
-        return IntStream.range(1, CELLS_COUNT + 1)
+        return IntStream.range(1, cellsCount + 1)
                 .boxed()
                 .collect(Collectors.toList());
     }
